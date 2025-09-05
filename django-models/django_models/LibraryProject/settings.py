1100236ed949cd608b65504e84334912241423e0
Bhubezyon <bhubezyon@gmail.com>
2025-09-05 04:56:25 -0700

    Django ORM relationship project

--- django-models/django_models/LibraryProject/settings.py
+++ django-models/django_models/LibraryProject/settings.py
@@ -1,3 +1,13 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'relationship_app' # Added the new app here
+]
+
 """
 Django settings for LibraryProject project.
 
