983b2b0db02c9dea49729962f13eb2d0c82cd9b8
Bhubezyon <bhubezyon@gmail.com>
2025-09-06 06:35:50 -0700

    Fix errors in views.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -78,11 +78,10 @@ from relationship_app.models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relation_app/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['librarian'] = self.object.librarian
         context['books'] = self.object.books_set.all()
         return context
