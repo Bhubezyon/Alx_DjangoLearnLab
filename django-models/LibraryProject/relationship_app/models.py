acd1dc32f55a1d6864fd09d5e5912bf12c2a466c
Bhubezyon <bhubezyon@gmail.com>
2025-09-06 06:56:12 -0700

    Add missing content in models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -60,3 +60,19 @@ class Meta:
         ("can_change_book", "Can change book"),
         ("can_delete_book", "Can delete book"),
     ]
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('librarian', 'Librarian'),
+        ('member', 'Member'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
