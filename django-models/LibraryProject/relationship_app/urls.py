9fb997cbe4f60c9e603f3e0d4c63aea4e00aa5cf
Bhubezyon <bhubezyon@gmail.com>
2025-09-06 06:50:17 -0700

    Add  missing ciontent in urls.py file

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -39,3 +39,13 @@ urlpatterns = [
     path('book/<int:pk>/edit/', views.edit_book, name='delete_book'),
     path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
+
+from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+]
