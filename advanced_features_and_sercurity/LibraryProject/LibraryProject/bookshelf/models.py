8b58ba31848c3389e83bbdeb28f56f2e904bfe46
Calvin Mogale <bhubezyon@gmail.com>
2025-09-12 19:32:15 +0200

    Add models.py in LibraryProject/Bookshelf

--- advanced_features_and_sercurity/LibraryProject/LibraryProject/bookshelf/models.py
+++ advanced_features_and_sercurity/LibraryProject/LibraryProject/bookshelf/models.py
@@ -8,7 +8,7 @@ class CustomUserManager(BaseUserManager):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
     def create_superuser(self, username, email, password=None, **extra_fields):
