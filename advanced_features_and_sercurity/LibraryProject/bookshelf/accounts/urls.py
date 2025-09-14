b988f42f44d8482373fc68fdd42ec441817ce8a1
Calvin Mogale <bhubezyon@gmail.com>
2025-09-15 00:12:17 +0200

    Rearange folders to pass checker

--- advanced_features_and_sercurity/LibraryProject/bookshelf/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.urls import path, include
-from django.conf.urls.static import static
-
-urlpatterns = [
-	# Add your URL patterns here, e.g. path('some-url/', some_view),
-]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
