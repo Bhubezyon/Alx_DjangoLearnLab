41c75df7f0f3dba8d92f5deeb0c0254dc60f7222
Calvin Mogale <bhubezyon@gmail.com>
2025-09-17 15:31:12 +0200

    Add models.py in LibraryProject/bookshelf

--- advanced_features_and_sercurity/LibraryProject/bookshelf/models.py
+++ advanced_features_and_sercurity/LibraryProject/bookshelf/models.py
@@ -1,59 +1,56 @@
-AUTH_USER_MODEL = 'accounts.CustomUser'
-# advanced_features_and_sercurity/LibraryProject/bookshelf/models.py
-
-from django.contrib.auth.models import AbstractUser, BaseUserManager
-from django.db import models
+from djando.db import models
+from django.contrib.auth.models import User, AbstractUser
+from django import settings
+from django.contrib.auth.models import UserManager, BaseUserManager
+# Create your models here.
+# class UserProfile(models.Model):
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Email is required')
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username must be set')
         email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
+        user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(email, password, **extra_fields)
-
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        return self.create_user(username, email, password, **extra_fields)
 
-        return self.create_user(email, password, **extra_fields)
-    
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username']
 
+    date_of_birth = models.DateField()
+    profile_photo = models.ImageField()
     objects = CustomUserManager()
-
     def __str__(self):
         return self.username
 
-# advanced_features_and_sercurity/LibraryProject/accounts/admin.py
-
-from django.db import models
-
+# Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    published_date = models.DateField()
-    isbn = models.CharField(max_length=13, unique=True)
-    pages = models.IntegerField()
-    cover_image = models.ImageField(upload_to='book_covers/', null=True, blank=True)
-    language = models.CharField(max_length=30)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.DateField()
+    isbn_number = models.CharField(max_length=13, unique=True)
+    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-    class Meta:
-        permissions = [
-            ("can_view", "Can view book"),
-            ("can_edit", "Can edit book"),
-            ("can_delete", "Can delete book"),
-        ]
+    def __str__(self):
+        return self.title
+
+class Meta:
+    permissions = [
+        ("can_view_book", "Can view book"),
+        ("can_create_book", "Can create book"),
+        ("can_edit_book", "Can edit book"),
+        ("can_delete_book", "Can delete book"),
+    ]
+
+    def __str__(self):
+        return self.title
+    
