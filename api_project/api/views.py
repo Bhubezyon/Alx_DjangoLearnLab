ac979885129e0c20ec35765cc64e0c8febe07891
Calvin Mogale <bhubezyon@gmail.com>
2025-09-20 00:45:27 -0700

    Save changes to pass checker

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,18 +1,14 @@
-from rest_framework.generics import ListAPIView
-from .models import Book
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import IsAuthenticated
+from .models import Book
 from .serializers import BookSerializer
-
-class BookList(viewsets.rest_framework.generics.ListAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-
-class BookViewSet(viewsets.Model.BookViewSet):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
+from rest_framework.generics import ListAPIView
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated] # Restricts access to authenticated users 
+    permission_classes = [IsAuthenticated] # Secure the endpoint
+
+class BookList(ListAPIView):
+    queryset = Book.object.all()
+    serializer_class = BookSerializer
