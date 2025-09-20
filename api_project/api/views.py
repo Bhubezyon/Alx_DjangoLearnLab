73dd4f15b4aca3dab0bbe6903451da6db490dc69
Calvin Mogale <bhubezyon@gmail.com>
2025-09-20 00:11:25 -0700

    Add missing content in views.py

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
 from rest_framework.generics import ListAPIView
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
