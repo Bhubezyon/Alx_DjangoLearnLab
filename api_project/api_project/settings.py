d05d42085f6c4c20a466df72e908ecbb8f0b5c72
Calvin Mogale <bhubezyon@gmail.com>
2025-09-20 00:55:42 -0700

    Save changes to pass checker

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -3,3 +3,12 @@ INSTALLED_APPS = [
     'rest_framework.authtoken'
     'api',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
