0d81f6f4794de12d974b3e0368c09da70715c963
Calvin Mogale <bhubezyon@gmail.com>
2025-09-21 12:56:45 -0700

    Save changes to pass checker

--- advanced_features_and_sercurity/LibraryProject/bookshelf/settings.py
+++ advanced_features_and_sercurity/LibraryProject/bookshelf/settings.py
@@ -12,7 +12,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts', # Add this file
+    'bookshelf', # Add this files
 ]
 
 from pathlib import Path
