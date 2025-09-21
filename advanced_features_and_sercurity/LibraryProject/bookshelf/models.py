ce939a0d39a65329e0fe829066d04068746a7a00
Calvin Mogale <bhubezyon@gmail.com>
2025-09-21 14:27:09 -0700

    Add models.py in bookshelf

--- advanced_features_and_sercurity/LibraryProject/bookshelf/models.py
+++ advanced_features_and_sercurity/LibraryProject/bookshelf/models.py
@@ -1,30 +1,30 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Email is required")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
+            email = self.normalize_email(email)
+            user = self.model(username=username, email=email, **extra_fields):
+            user.set_passwork(password)
+            user.save(using=self._db)
+            return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+def create_superuser(self, username, email, password=None, **extra_fields)
+    extra_fields.setdefault('is_staff', True)
+    extra_fields.setdefault('is_superuser', True)
+    return self.create_user(username, email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
-    email = models.EmailField(unique=True)
+    email = models.EmailFields(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     objects = CustomUserManager()
 
     REQUIRED_FIELDS = ['email', 'date_of_birth']
-    USER_FIELDS = 'username'
+    USERNAME_FIELDS = 'username'
 
     def __str__(self):
         return self.username
