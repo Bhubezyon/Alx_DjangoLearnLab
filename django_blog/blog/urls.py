d132ae8102a9cbb85bdec3ba85217c60a3eef601
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 10:43:06 -0700

    Add comment class in blog/urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -14,6 +14,16 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
 
+from .views import (
+    CommentCreateView, CommentUpdateView, CommentDeleteView,
+)
+
+urlpatterns += [
+    path("post/<int:post_pk>/comment/new/", CommentCreateView.as_view(), name="comment_create"),
+    path("comment/<int:pk>/update/", CommentUpdateView.as_view(), name="comment_update"),
+    path("comment/<int:pk>/delete/", CommentDeleteView.as_view(), name="comment_delete"),
+]
+
 urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
