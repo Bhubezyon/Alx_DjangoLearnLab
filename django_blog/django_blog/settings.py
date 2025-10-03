471514980fbe8df68303a9589a3eb02eaa1ef4a7
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 05:49:53 -0700

    Add DATABASE in django_blog/settings.py

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -1,3 +1,17 @@
+import os
+
+DATABAES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT'),
+
+    }
+}
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
