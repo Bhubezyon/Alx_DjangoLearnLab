d54ccc9d6ee5a43d64b1134616e23fe9e2671ff2
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 05:54:27 -0700

    Save chenges in django_blog/settings.py to pass checker

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -3,11 +3,11 @@ import os
 DATABAES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'your_db_name',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
 
     }
 }
