b636900ff2b7310a865d837d5be7e2ef45b6a924
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 06:54:00 -0700

    Django blog with authentication system

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,7 +1,10 @@
-# blog/urls.py
 from django.urls import path
-from . import views
+from django.contrib.auth import views as auth_views
+from .views import register_view, profile_view
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="registration/logout.html"), name="logout"),
+    path("register/", register_view, name="register"),
+    path("profile/", profile_view, name="profile"),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,29 @@
-# blog/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CostumUserCreationForm
+from django.contrib import messages
 
-def home(request):
-    return render(request, 'blog/base.html')
+def register_view(request):
+    if request.method == 'POST':
+        form = CostumUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('profile')
+        else:
+            messages.error(request, "Unsuccessful registration. Invalid information.")
+    else:
+        form = CostumUserCreationForm()
+    return render(request, "registration/register.html", {"form": form})
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        request.user.email = request.POST.get('email', request.user.email)
+        request.user.save()
+        messages.success(request, "Profile updated successfully.")
+        return render(request, "registration/profile.html", {"user": request.user})
+
