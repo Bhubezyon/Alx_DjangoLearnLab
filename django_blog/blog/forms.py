f8c0138859305ffc83d835d85c5b022065e0e213
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 11:04:15 -0700

    Add tags , widgets in blog/forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import Comment
+from .models import Post
+from taggit.forms import TagWidget
 
 class CostumUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -20,3 +22,11 @@ class CommentForm(forms.ModelForm):
         if not content or content.strip() == "":
             raise forms.ValidationError("Comment cannot be empty")
         return content
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'content', 'tags']
+        widgets = {
+            'tags': TagWidget(),
+        }
