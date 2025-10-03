ca2995cd194749c3abcaf7e22f0d71d0bca43089
Calvin Mogale <bhubezyon@gmail.com>
2025-10-03 10:38:26 -0700

    add comment class in models/forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django import Comment
 
 class CostumUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,14 @@ class CostumUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+    def clean_content(self):
+        content = self.cleaned_data.get('content')
+        if not content or content.strip() == "":
+            raise forms.ValidationError("Comment cannot be empty")
+        return content
