fb8617c04cba6e7de6b73b362196d6a6e26cc719
Calvin Mogale <bhubezyon@gmail.com>
2025-10-05 12:40:54 -0700

    Save changes to pass checker

--- django_blog/blog/static/blog/js/script.js
+++ django_blog/blog/static/blog/js/script.js
@@ -1,2 +1,19 @@
 // blog/static/blog/js/script.js
 console.log("Blog JS loaded - script.js:2");
+
+AuthenticatorAssertionResponse.apply(this, arguments);
+console.log("Blog JS loaded - script.js:5");
+
+// If you want to inject this CSS via JavaScript, use the following:
+const style = document.createElement('style');
+style.textContent = `
+.auth-form {
+    max-width: 300px;
+    margin: auto;
+    padding: 20px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    background-color: #f9f9f9;
+}
+`;
+document.head.appendChild(style);
