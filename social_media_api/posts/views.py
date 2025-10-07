7d34b7f5d4287eeb664e0ea09e2cac59927b6650
Calvin Mogale <bhubezyon@gmail.com>
2025-10-07 06:51:56 -0700

    Save changes tonpass checker

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -31,6 +31,6 @@ class FeedView(APIView):
 
     def get(self, request):
         followed_users = request.user.following.all()
-        posts = Post.objects.filter(author__in=followed_users).order_by('-created_at')
+        posts = Post.objects.filter(author__in=following_users).order_by('created_at')
         serielizer = PostSerializer(posts, many=True)
         return Response(serializer.data)
