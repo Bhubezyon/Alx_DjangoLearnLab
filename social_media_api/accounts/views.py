c03836fab91abe7d5becc52f786527caa9d7843c
Calvin Mogale <bhubezyon@gmail.com>
2025-10-07 06:36:05 -0700

    Save changes to pass checker

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response    
-from rest_framework import status, permissions
+from rest_framework import status, permissions, generics
 from django.contrib.auth import get_user_model
 from .serializers import RegisterSerializer, LoginSerializer
-from rest_framework.permissions import AllowAny 
+from rest_framework.permissions import AllowAny
+
 
 User = get_user_model()
 
@@ -48,3 +49,27 @@ class UnfollowUserView(APIView):
             return Response({'message': f'You have unfollowed {target_user.username}'}, status=status.HTTP_200_OK)
         except User.DoesNotExist:
             return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+
+class FollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permission.IsAuthenticated]
+
+    def post(self, request, user_id):
+        try:
+            target_user = self.get_queryset().get(id=user_id)
+            request.user.following.add(target_user)
+            return Response({'message': f'You are now following {target_user.username}'}, status=status.HHTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HHTP_404_NOT_FOUND)
+
+class UnfollowUserView(generics.GenericAPIView):
+    queryset = CustomUser.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        try:
+            target_user = self.get_queryset().get(id=user_id)
+            request.user.following.remove(target_user)
+            return Response({'mesage': f'You have unfollowed {target_user.username}'}, status=status.HHTP_200_OK)
+        except CustomUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HHTP_404_NOT_FOUND)
